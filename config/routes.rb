af154dd2ac9084ed38d968e87859fa76e39197a9
vanvurl <vanvurl@gmail.com>
2017-10-07 08:07:59 +0000

    User Search Added

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "user/registrations"}
   resources :user_stocks, except: [:show, :edit, :update]
   root 'welcome#index'
+  resources :users, only: [:show]
+  resources :friendships
   get 'my_portfolio', to: "users#my_portfolio"
   
   get 'search_stocks', to: "stocks#search"
   
   get 'my_friends', to: 'users#my_friends'
   
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: "users#add_friend"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
